c9a738e649b77253266b59d9a0c7648b4ab1e3ac
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-10-28 17:33:18 +0100

    Add type name access and move access/aggregate access into module

--- lib/sandthorn_driver_sequel/access.rb
+++ lib/sandthorn_driver_sequel/access.rb
@@ -1,9 +1,12 @@
-class Access
-  def initialize(storage)
-    @storage = storage
+module SandthornDriverSequel
+  class Access
+    def initialize(storage)
+      @storage = storage
+    end
+
+    private
+
+    attr_reader :storage
   end
 
-  private
-
-  attr_reader :storage
 end
--- lib/sandthorn_driver_sequel/aggregate_access.rb
+++ lib/sandthorn_driver_sequel/aggregate_access.rb
@@ -1,25 +1,30 @@
-require File.expand_path(File.dirname(__FILE__) + '/access.rb')
-class AggregateAccess < Access
+module SandthornDriverSequel
+  class AggregateAccess < Access
 
-  def find_or_register(aggregate_id, aggregate_type)
-    if aggregate = find_by_aggregate_id(aggregate_id)
-      aggregate
-    else
-      register_aggregate(aggregate_id, aggregate_type)
+    def find_or_register(aggregate_id, aggregate_type)
+      if aggregate = find_by_aggregate_id(aggregate_id)
+        aggregate
+      else
+        register_aggregate(aggregate_id, aggregate_type)
+      end
     end
-  end
 
-  def register_aggregate(aggregate_id, aggregate_type)
-    id = storage.aggregates.insert(aggregate_id: aggregate_id, aggregate_type: aggregate_type.to_s)
-    find(id)
-  end
+    def register_aggregate(aggregate_id, aggregate_type)
+      id = storage.aggregates.insert(aggregate_id: aggregate_id, aggregate_type: aggregate_type.to_s)
+      find(id)
+    end
 
-  def find(id)
-    storage.aggregates[id]
-  end
+    def find(id)
+      storage.aggregates[id]
+    end
 
-  def find_by_aggregate_id(aggregate_id)
-    storage.aggregates.first(aggregate_id: aggregate_id)
-  end
+    def find_by_aggregate_id(aggregate_id)
+      storage.aggregates.first(aggregate_id: aggregate_id)
+    end
 
+    def type_names
+      storage.aggregates.select(:aggregate_type).distinct.select_map(:aggregate_type)
+    end
+
+  end
 end
