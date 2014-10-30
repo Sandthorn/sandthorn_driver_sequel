052697518bdb95cffa9d52c4362403971e03c579
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-10-30 12:37:42 +0100

    Storing old code for reference is what git is for. Delete commented code.

--- lib/sandthorn_driver_sequel/sequel_driver.rb
+++ lib/sandthorn_driver_sequel/sequel_driver.rb
@@ -18,23 +18,3 @@ module SandthornDriverSequel
 
   end
 end
-
-
-# module SandthornDriverSequel
-#   class SequelDriver
-#     def initialize args = {}
-#       @url = args.fetch(:url)
-#       Sequel.default_timezone = :utc
-#     end
-#     def execute &block
-#       Sequel.connect(@url) { |db| return block.call db}
-#     end
-#     def execute_in_transaction &block
-#       Sequel.connect(@url) do |db|
-#         db.transaction do
-#           return block.call db
-#         end
-#       end
-#     end 
-#   end
-# end
