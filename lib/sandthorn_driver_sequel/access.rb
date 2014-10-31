12f7d6016bbf57f9916f6392e6343a9d3a08a7ee
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-10-31 11:11:31 +0100

    Add comments to Access::Base

--- lib/sandthorn_driver_sequel/access.rb
+++ lib/sandthorn_driver_sequel/access.rb
@@ -1,6 +1,9 @@
 module SandthornDriverSequel
   module Access
     class Base
+      # = Access::Base
+      # Inheriting classes use +storage+ to provide access to a
+      # particular database model/table.
       def initialize(storage)
         @storage = storage
       end
